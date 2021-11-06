0906c7b095b67c30a574954f393eb3dcec566f98
KatiaDev <ecaterina.popa.2021@gmail.com>
2021-11-06 17:44:52 +0200

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="container">
+          <div className="display">
+            <span className="cursor" />
+          </div>
+          <button className="btn">AC</button>
+          <button className="btn">C</button>
+          <button className="btn">x</button>
+          <button className="btn">/</button>
+          <button className="btn">7</button>
+          <button className="btn">8</button>
+          <button className="btn">9</button>
+          <button className="btn">+</button>
+          <button className="btn">4</button>
+          <button className="btn">5</button>
+          <button className="btn">6</button>
+          <button className="btn">-</button>
+          <button className="btn">1</button>
+          <button className="btn">2</button>
+          <button className="btn">3</button>
+          <button className="btn zero">0</button>
+          <button className="btn">.</button>
+          <button className="btn eq">=</button>
+        </div>
+      </div>
     </div>
   );
 }

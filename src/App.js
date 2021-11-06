ea1e1032d1487f49c381f4a6991f3bc71c6a7ab0
KatiaDev <ecaterina.popa.2021@gmail.com>
2021-11-07 00:28:15 +0200

    Redux hooks example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "./actions/counterActions";
 
-const App = (props) => {
+const App = () => {
+  const dispatch = useDispatch();
+  const { count } = useSelector((state) => state);
+
   const handleIncrease = () => {
-    props.increment();
+    dispatch(increment());
   };
 
   const handleDecrease = () => {
-    props.decrement();
+    dispatch(decrement());
   };
 
   return (
     <div className="box">
       <h1>Counter</h1>
-      <p>Count: {props.count}</p>
+      <p>Count: {count}</p>
 
       <div>
         <button
@@ -36,22 +39,4 @@ const App = (props) => {
   );
 };
 
-// The mapStateToProps function specifies which portion of the
-// state tree this component needs to receive. In this case,
-// since our redux store is only storing the value of the count,
-// this component receives the whole state. In a more complex
-// redux application, though, it would receive only the relevant
-// parts it needs from the state object.
-
-const mapStateToProps = (state) => {
-  return {
-    count: state.count,
-  };
-};
-// The connect function is called in order to make this component aware
-// of the rest of the redux architecture. Without this, this component
-// is only a dumb React component. We pass in all of the functions that
-// are reliant on Redux, along with the component itself, so that Redux
-// makes itself known to this component.
-
-export default connect(mapStateToProps, { increment, decrement })(App);
+export default App;
